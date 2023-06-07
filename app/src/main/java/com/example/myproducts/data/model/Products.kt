f12258a6bc120df1596079a3a31c71e280c4e0a0
gitam2869 <gautamhelange025@gmail.com>
2023-06-07 23:16:53 +0530

    Project setup, added dependencies

---
+++ app/src/main/java/com/example/myproducts/data/model/Products.kt
@@ -0,0 +1,3 @@
+package com.example.runningtrackerapp.data.model
+
+class Products : ArrayList<ProductItem>()
---
+++ app/src/main/java/com/example/myproducts/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.myproducts.utils
+
+object Constants {
+
+    const val BASE_URL = "https://app.getswipe.in/"
+}
